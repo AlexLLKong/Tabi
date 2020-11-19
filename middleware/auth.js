c6c2e5747e6668fb341b634e3b748869870d5fbb
AlexLLKong <alexkonghockey@gmail.com>
2020-11-19 05:47:34 -0800

    Fix Token is not valid issue
    [Ticket: X]

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,15 @@ import jwt from 'jsonwebtoken'
 
 const auth = (req, res, next) => {
 	const token = req.header('x-auth-token')
-
+	const secret =
+		process.env.NODE_ENV === 'production'
+			? process.env.jwtSecret
+			: config.get('jwtSecret')
 	if (!token) {
 		return res.status(401).json({ msg: 'No token authorization denied' })
 	}
 	try {
-		const decoded = jwt.verify(token, config.get('jwtSecret'))
+		const decoded = jwt.verify(token, secret)
 		req.user = decoded
 		next()
 	} catch (e) {
